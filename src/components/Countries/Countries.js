13d1f68c61465b285441fb3cd19c8a6f05f4bbc8
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-18 00:09:40 +0600

    Unique key

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,7 @@
 // rsc
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -14,14 +15,15 @@ const Countries = () => {
     return (
         <div>
             <h2>Countries: {countries.length}</h2>
-
-            {
-                // countries.map(country => console.log(country))
-            }
-            {
-                // countries.map(country => console.log(country))
-                countries.map(country => <Country country={country}></Country>)
-            }
+            <div className='countries-container'>
+                {
+                    // countries.map(country => console.log(country))
+                    countries.map(country => <Country 
+                        country={country}
+                        key={country.cca3}
+                        ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area, population, region, name} = props.country;
+    const {area, population, region, name, flags, cca3} = props.country;
+    // console.log(props.country);
     return (
         <div className='country'>
             <h2>Country Name: {name.common}</h2>
+            <img src={flags.png}/>
             <p>Population: {population}</p>
             <p><small>Region: {region}</small></p>
             <p><small>Area: {area}</small></p>
+            <p><small>CCA3 Key: {cca3}</small></p>
         </div>
     );
 };
