4f946f228a408934872d8b19030815cdf1ccd5b0
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-17 15:34:45 +0600

    Folder structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Country/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Header></Header>
+      {/* <LoadCountries></LoadCountries> */}
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
 // *********** Component ***********
-function LoadCountries(){
+/* function LoadCountries(){
   const [countries, setCountries] = useState([]);
 
   useEffect( () =>{
@@ -41,5 +47,5 @@ function Country(props){
       <h4>Population: {props.population}</h4>
     </div>
   )
-}
+} */
 export default App;
---
+++ src/components/Country/Countries.js
@@ -0,0 +1,12 @@
+// rsc
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countries</h2>
+        </div>
+    );
+};
+
+export default Countries;
